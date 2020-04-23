bfc1d74f493d909f04b77873f75dc0ad28f88d5c
Antoine Venco <antoine@dataveyes.com>
2020-04-23 19:00:01 +0200

    Adding sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return <div className='App'>Hello World!!!</div>;
-  }
-}
+import HomePage from "./homepage.component";
+
+const App = () => {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
+};
 
 export default App;
